ccf465fded4996957ade2075e0de6e252a0ba449
iwongg1 <iwong1@ufl.edu>
2025-11-12 16:43:49 -0500

    change names

--- calculator.py
+++ calculator.py
@@ -23,7 +23,7 @@ def hypotenuse(a,b):
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
 def mul(a, b):
@@ -36,7 +36,7 @@ def div(a, b):
         raise ZeroDivisionError("Cannot divide by 0!")
     return result
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 1:
         raise ValueError("Base must be larger than 1!")
     if b <= 0:
