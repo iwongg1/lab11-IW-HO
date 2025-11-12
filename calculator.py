6a68eeb989d574dfb80932f98f51284c40ff51aa
Holly Overholser <hollyoverholser011@gmail.com>
2025-11-12 15:35:32 -0500

    merged calculator

--- calculator.py
+++ calculator.py
@@ -18,23 +18,21 @@ def hypotenuse(a,b):
     return math.hypot(a,b)
 
 def add(a, b):
+    return a + b
 
-    def add(a, b):
-        return a + b
+def sub(a, b):
+    return a - b
 
-    def sub(a, b):
-        return a - b
+def mul(a, b):
+    return a * b
 
-    def mul(a, b):
-        return a * b
+def log(a, b):
+    if a <= 0:
+        raise ValueError("Base must be larger than 0!")
+    return math.log(b, a)
 
-    def log(a, b):
-        if a <= 0:
-            raise ValueError("Base must be larger than 0!")
-        return math.log(b, a)
-
-    def exp(a, b):
-        return a ** b
+def exp(a, b):
+    return a ** b
 
 
 
