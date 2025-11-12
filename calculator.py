e48cf3228767ff75c1d06d7c1e8c5e5aa583ebf5
Holly Overholser <hollyoverholser011@gmail.com>
2025-11-12 15:24:21 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -5,7 +5,31 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+import math
+
 def add(a, b): 
-    pass
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def div(a,b):
+    if a == 0:
+        raise ZeroDivisionError("Denominator cannot be 0.")
+
+    return b / a
+
+def log(a,b)
+    if a <= 0:
+        raise ValueError("Base must be larger than 0.")
+
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
+
 
 
